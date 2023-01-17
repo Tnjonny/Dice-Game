b79962bc37975b2a74488f9156740f7822d7b585
Tnjonny <79794963+Tnjonny@users.noreply.github.com>
2023-01-17 22:16:22 +0000

    Added Toggle element

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const players0 = document.querySelector('.player--0');
+const players1 = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+
 const currentPlayer0 = document.getElementById('current--0');
 const currentPlayer1 = document.getElementById('current--1');
 
@@ -15,7 +19,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 dice.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayers = 0;
 
 btnRoll.addEventListener('click', function () {
   const diceEl = Math.floor(Math.random() * 6) + 1;
@@ -24,8 +30,13 @@ btnRoll.addEventListener('click', function () {
 
   if (diceEl !== 1) {
     currentScore += diceEl;
-    currentPlayer0.textContent = currentScore;
+    document.getElementById(`current--${activePlayers}`).textContent =
+      currentScore;
   } else {
-    currentPlayer1.textContent = currentScore;
+    document.getElementById(`current--${activePlayers}`).textContent = 0;
+    activePlayers = activePlayers === 0 ? 1 : 0;
+    currentScore = 0;
+    players0.classList.toggle('player--active');
+    players1.classList.toggle('player--active');
   }
 });
